2be3ed854b7ff351ce782cb7191b92322e39d7a7
Patrick DANG <patrick.dang.10@gmail.com>
2022-02-16 15:38:11 +0100

    Add Options

--- src/Entity/Property.php
+++ src/Entity/Property.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use Cocur\Slugify\Slugify;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use App\Repository\PropertyRepository;
 use Symfony\Component\Validator\Constraints as Assert;
@@ -98,9 +100,15 @@ class Property
      */
     private $created_at;
 
+    /**
+     * @ORM\ManyToMany(targetEntity=Option::class, inversedBy="properties")
+     */
+    private $options;
+
     public function __construct()
     {
         $this->setCreatedAt(new \DateTime);
+        $this->options = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -294,5 +302,32 @@ class Property
     public function getHeatsIds()
     {
         return array_keys(self::HEAT);
+    }
+
+    /**
+     * @return Collection|Option[]
+     */
+    public function getOptions(): Collection
+    {
+        return $this->options;
+    }
+
+    public function addOption(Option $option): self
+    {
+        if (!$this->options->contains($option)) {
+            $this->options[] = $option;
+            $option->addProperty($this);
+        }
+
+        return $this;
+    }
+
+    public function removeOption(Option $option): self
+    {
+        if ($this->options->removeElement($option)) {
+            $option->removeProperty($this);
+        }
+
+        return $this;
     }
 }
--- src/Form/PropertySearchType.php
+++ src/Form/PropertySearchType.php
@@ -2,12 +2,14 @@
 
 namespace App\Form;
 
+use App\Entity\Option;
 use App\Entity\PropertySearch;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 
 class PropertySearchType extends AbstractType
 {
@@ -27,6 +29,13 @@ class PropertySearchType extends AbstractType
                 'attr' => [
                     'placeholder' => 'Surface minimum'
                 ]
+            ])
+            ->add('options', EntityType::class, [
+                'required' => false,
+                'label' => false,
+                'class' => Option::class,
+                'choice_label' => 'name',
+                'multiple' => true
             ]);
     }
 
